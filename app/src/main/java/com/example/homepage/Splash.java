f724567b7750899be2b2347656e89f5cdf7bf6d3
MR-Strange2000 <krishsachin2000@gmail.com>
2021-05-20 21:33:43 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/homepage/Splash.java
@@ -0,0 +1,44 @@
+package com.example.homepage;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+
+public class Splash extends AppCompatActivity {
+    private static int SPLASH_SCREEN = 6000;
+    Animation topanim;
+    CircleImageView img;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+        img = (CircleImageView)findViewById(R.id.dp1);
+        topanim = AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        img.setAnimation(topanim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(Splash.this,Start.class);
+                startActivity(i);
+                finish();
+            }
+        },SPLASH_SCREEN);
+
+    }
+
+
+
+
+}
