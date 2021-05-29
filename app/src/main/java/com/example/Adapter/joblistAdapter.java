281e2755feab9b79c270d66c92db177de5a36cbf
MR-Strange2000 <krishsachin2000@gmail.com>
2021-05-29 12:25:31 +0530

    ananthe

--- app/src/main/java/com/example/Adapter/joblistAdapter.java
+++ app/src/main/java/com/example/Adapter/joblistAdapter.java
@@ -1,16 +1,22 @@
-/*package com.example.Adapter;
+package com.example.Adapter;
 
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import android.view.View;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.homepage.Browse;
 import com.example.homepage.R;
 import com.example.homepage.post_job_format;
 import com.google.firebase.database.core.Context;
-import com.google.firebase.database.core.view.View;
 
+
+import java.text.BreakIterator;
 import java.util.List;
 
 public class joblistAdapter extends RecyclerView.Adapter<joblistAdapter.MyViewHolder> {
@@ -24,7 +30,7 @@ public class joblistAdapter extends RecyclerView.Adapter<joblistAdapter.MyViewHo
         this.classPath = classPath;
     }
 
-    public static post_job_format getPooja(int id) {
+    public static post_job_format getJobs(int id) {
         return jobs.get(id);
     }
 
@@ -32,14 +38,15 @@ public class joblistAdapter extends RecyclerView.Adapter<joblistAdapter.MyViewHo
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
 
-        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.pooja_list_items, viewGroup, false);
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.job_list_items, viewGroup, false);
         return new MyViewHolder(view);
-    }
+    }*/
+
 
     @NonNull
     @Override
-    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
-        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.pooja_list_items, viewGroup, false);
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.job_list_items, parent, false);
         return new MyViewHolder(view);
 
     }
@@ -47,13 +54,13 @@ public class joblistAdapter extends RecyclerView.Adapter<joblistAdapter.MyViewHo
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder viewHolder, int i) {
         final int id = i;
-        viewHolder.poojaName.setText(poojas.get(i).getTitle());
-        viewHolder.detailsText.setText(poojas.get(i).getDate() + " . " + poojas.get(i).getTime() + " . " + poojas.get(i).getBid() + " bids");
+        viewHolder.jobName.setText(jobs.get(i).getName());
+        viewHolder.detailsText.setText(jobs.get(i).getCatagory() + " . " + jobs.get(i).getPrice() + " . " + jobs.get(i).getBid() + " bids");
 
         viewHolder.relativeLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(classPath.equals("selectedPooja"))
+                if(classPath.equals("selectedjob"))
                     context.startActivity(new Intent(context, selectedPooja.class).putExtra("id", id));
                 else
                     context.startActivity(new Intent(context, selectedPoojaPandit.class).putExtra("id", id));
@@ -63,21 +70,21 @@ public class joblistAdapter extends RecyclerView.Adapter<joblistAdapter.MyViewHo
 
     @Override
     public int getItemCount() {
-        return poojas.size();
+        return jobs.size();
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder{
 
-        public TextView poojaName, detailsText;
+        public TextView jobName, detailsText;
         public RelativeLayout relativeLayout;
+
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
 
-            poojaName = itemView.findViewById(R.id.poojaName);
+            jobName = itemView.findViewById(R.id.jobName);
             detailsText = itemView.findViewById(R.id.detailsText);
             relativeLayout = itemView.findViewById(R.id.itemComponent);
         }
     }
 }
-*/
--- app/src/main/java/com/example/homepage/Browse.java
+++ app/src/main/java/com/example/homepage/Browse.java
@@ -1,42 +1,41 @@
 package com.example.homepage;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.View;
 import android.widget.EditText;
-import android.widget.GridLayout;
 import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.Adapter.joblistAdapter;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
+import com.google.firebase.database.core.Context;
 import java.util.ArrayList;
+import java.util.List;
 
 import static com.example.homepage.R.layout.add;
 import static com.example.homepage.R.layout.home;
 import static com.example.homepage.R.layout.search;
+import com.example.Adapter.joblistAdapter;
 
 public class Browse extends AppCompatActivity {
     private EditText searchBox;
     private RecyclerView recyclerView;
     private RecyclerView.Adapter adapter;
 
-    //public List<pooja> list = new ArrayList<>();
-    //public List<pooja> newList = new ArrayList<>();
-    public static String classPath = "selectedPooja";
+    public List<post_job_format> list = new ArrayList<>();
+    public List<post_job_format> newList = new ArrayList<>();
+    public static String classPath = "selectedjob";
     //GridLayout mainGridLayout;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -79,41 +78,41 @@ public class Browse extends AppCompatActivity {
     }
 
     private void getDataFromFirebase(DatabaseReference databaseReference) {
-       // list.clear();
+        list.clear();
         databaseReference.addValueEventListener(
                 new ValueEventListener() {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
                         if(dataSnapshot == null)
-                            //Toast.makeText(poojaList.this, "No data found! Add a Puja on home page", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(Browse.this, "No data found! Add a Puja on home page", Toast.LENGTH_SHORT).show();
                         for (DataSnapshot dsp : dataSnapshot.getChildren()) {
-                            //list.add(dsp.getValue(pooja.class));
+                            list.add(dsp.getValue(post_job_format.class));
                         }
-                       // adapter = new poojaListAdapter(poojaList.this, list, classPath);
+                        adapter = new joblistAdapter(Browse.this, list, classPath);
                         adapter.notifyDataSetChanged();
                         recyclerView.setAdapter(adapter);
                     }
 
                     @Override
                     public void onCancelled(DatabaseError databaseError) {
-                        //Toast.makeText(poojaList.this, "Server Error! Check your Internet Connection", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(Browse.this, "Server Error! Check your Internet Connection", Toast.LENGTH_SHORT).show();
                     }
                 });
     }
 
     private void getDataFromSearchQuery(String searchString) {
-       // newList.clear();
-       /* for(int i=0; i< list.size(); i++){
-            post_details_format object = list.get(i);
-            if(object.getName().toLowerCase().trim().contains(searchString) || object.getPincode().toLowerCase().trim().contains(searchString)){
-                newList.add(object);*/
+       newList.clear();
+        for(int i=0; i< list.size(); i++){
+            post_job_format object = list.get(i);
+            if(object.getName().toLowerCase().trim().contains(searchString) || object.getCatagory().toLowerCase().trim().contains(searchString)){
+                newList.add(object);
             }
         }
-        //adapter = new poojaListAdapter(poojaList.this, newList, classPath);
-        //adapter.notifyDataSetChanged();
-       // recyclerView.setAdapter(adapter);
-    //}
-   // }
+        adapter = new joblistAdapter(Browse.this, newList, classPath);
+        adapter.notifyDataSetChanged();
+        recyclerView.setAdapter(adapter);
+    }
+    }
 
 
 
