f7ccf565fc9bfd6b9dab151e2b6a273b25b4a13f
MR-Strange2000 <krishsachin2000@gmail.com>
2021-05-30 21:25:47 +0530

    All Jobs

--- app/src/main/java/com/example/Adapter/open_adapter.java
+++ app/src/main/java/com/example/Adapter/open_adapter.java
@@ -1,29 +1,33 @@
 package com.example.Adapter;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.view.menu.ShowableListMenu;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.homepage.R;
 import com.example.homepage.all_task_disp;
 import com.example.homepage.post_job_format;
+import com.example.homepage.selectedjob;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static com.example.homepage.all_task_disp.ClassPath;
+
 public class open_adapter extends RecyclerView.Adapter<open_adapter.MyviewHolder> {
+    all_task_disp context;
 
     private all_task_disp activity;
     public List<post_job_format> list = new ArrayList<>();
 
 
-    public open_adapter(all_task_disp activity , List<post_job_format> list){
+    public open_adapter(all_task_disp activity, List<post_job_format> list, String classPath){
+        this.context = context;
         this.activity = activity;
         this.list = list;
     }
@@ -36,8 +40,22 @@ public class open_adapter extends RecyclerView.Adapter<open_adapter.MyviewHolder
 
     @Override
     public void onBindViewHolder(@NonNull  open_adapter.MyviewHolder holder, int position) {
+        final int id = position;
         holder.title.setText(list.get(position).getName());
         holder.desc.setText(list.get(position).getDescription());
+        holder.relativeLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ClassPath.equals("JobSelected")) {
+
+                    context.startActivity(new Intent(context, selectedjob.class).putExtra("id", id));
+                }
+               /* else {
+                    Toast.makeText(joblistAdapter.this, "Server Error! Check your Internet Connection", Toast.LENGTH_SHORT).show();
+                }*/
+                //context.startActivity(new Intent(context, selectedPoojaPandit.class).putExtra("id", id));
+            }
+        });
     }
 
     @Override
@@ -46,11 +64,13 @@ public class open_adapter extends RecyclerView.Adapter<open_adapter.MyviewHolder
     }
 
     public  static class MyviewHolder extends  RecyclerView.ViewHolder {
+        public View relativeLayout;
         TextView title , desc;
         public MyviewHolder(@NonNull  View itemView) {
             super(itemView);
             title = itemView.findViewById(R.id.jobName);
             desc = itemView.findViewById(R.id.detailsText);
+            relativeLayout = itemView.findViewById(R.id.itemComponent);
         }
     }
 }
--- app/src/main/java/com/example/homepage/all_task_disp.java
+++ app/src/main/java/com/example/homepage/all_task_disp.java
@@ -6,11 +6,8 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.widget.Adapter;
-import android.widget.EditText;
 import android.widget.Toast;
 
-import com.example.Adapter.joblistAdapter;
 import com.example.Adapter.open_adapter;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -23,9 +20,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class all_task_disp extends AppCompatActivity {
-    RecyclerView rec;
+    private RecyclerView rec;
+    private RecyclerView.Adapter adapter;
     private FirebaseDatabase firebase;
-    private DatabaseReference databaseReference;
+    private DatabaseReference databaseReference,db;
+    public static String ClassPath = "JobSelected";
     private open_adapter open;
     private List<post_job_format> list;
     @Override
@@ -39,25 +38,47 @@ public class all_task_disp extends AppCompatActivity {
 
         firebase = FirebaseDatabase.getInstance();
         databaseReference = firebase.getReference("project");
+        db = firebase.getReference("project_details");
         list = new ArrayList<>();
-        open = new open_adapter(this , list);
+        open = new open_adapter(this , list, ClassPath);
         showData();
     }
 
     private void showData(){
-        databaseReference.addValueEventListener(new ValueEventListener() {
+        db.child(FirebaseAuth.getInstance().getCurrentUser().getUid()).addValueEventListener(new ValueEventListener() {
             @Override
-            public void onDataChange(@NonNull  DataSnapshot snapshot) {
-                for(DataSnapshot snap: snapshot.getChildren()){
-                    post_job_format user = snap.getValue(post_job_format.class);
-                    list.add(user);
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists() && snapshot.getChildrenCount()>0){
+                    post_job_format user = snapshot.getValue(post_job_format.class);
+                    String path = user.rid;
+
+
+                    databaseReference.child(FirebaseAuth.getInstance().getCurrentUser().getUid()+path).addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull  DataSnapshot snapshot) {
+                            for(DataSnapshot snap: snapshot.getChildren()){
+                                post_job_format user = snap.getValue(post_job_format.class);
+                                list.add(user);
+                            }
+                            adapter = new open_adapter(all_task_disp.this,list,ClassPath);
+                            adapter.notifyDataSetChanged();
+                            rec.setAdapter(adapter);
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull  DatabaseError error) {
+                            Toast.makeText(all_task_disp.this, "Cannot Fetch", Toast.LENGTH_SHORT).show();
+                        }
+                    });
                 }
+
             }
 
             @Override
-            public void onCancelled(@NonNull  DatabaseError error) {
-                Toast.makeText(all_task_disp.this, "Cannot Fetch", Toast.LENGTH_SHORT).show();
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
+
     }
 }
